9c28cffc53c61e9136713c2da184a31ec188c4f7
ksh3 <info@loft.tokyo>
2020-03-02 23:16:14 +0900

    update

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import pytest
 from starlette.testclient import TestClient
+from databases import Database, DatabaseURL
 
 from main import app
-from src import store
+from src import store, config
 
 
 class TestView:
@@ -14,12 +15,30 @@ class TestView:
             )
             assert response.status_code == 200
 
+    def test_retrieve(self):
+        with TestClient(app) as client:
+            response = client.get("/v1/robots/1")
+            assert response.status_code == 200
+
 
 class TestModel:
 
+    def setup_method(self, method):
+        store.database.url = DatabaseURL(config.TEST_DATABASE_URL)
+        self.test_db: Database = store.database
+
     @pytest.mark.asyncio
     async def test_retrieve(self):
-        await store.database.connect()
+        await self.test_db.connect()
         res = await store.Robot.retrieve({'robot_id': 1})
         assert res.name == 'GUIDO'
-        await store.database.disconnect()
+        await self.test_db.disconnect()
+
+    @pytest.mark.asyncio
+    async def test_count(self):
+        await self.test_db.connect()
+        old_robot = await store.Robot.retrieve({'robot_id': 1})
+        await old_robot.count_ref()
+        new_robot = await store.Robot.retrieve({'robot_id': 1})
+        assert old_robot.count + 1 == new_robot.count
+        await self.test_db.disconnect()
